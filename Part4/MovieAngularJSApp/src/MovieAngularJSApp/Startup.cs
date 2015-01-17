c0f2e3ea7dcf4ed1d00b196e4849705930b62a8e
StephenWalther <Stephen.Walther@superexpert.com>
2015-01-16 21:07:13 -0800

    change db connection string

--- Part4/MovieAngularJSApp/src/MovieAngularJSApp/Startup.cs
+++ Part4/MovieAngularJSApp/src/MovieAngularJSApp/Startup.cs
@@ -2,23 +2,34 @@
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Data.Entity;
 using MovieAngularJSApp.Models;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.ConfigurationModel;
+
+
 
 namespace MovieAngularJSApp
 {
 	public class Startup
 	{
+		public Startup(IHostingEnvironment env)
+		{
+			// Setup configuration sources.
+			Configuration = new Configuration()
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+		}
+
+		public IConfiguration Configuration { get; set; }
+
 
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc();
 
 			// Register Entity Framework
-			services.AddEntityFramework()
+			services.AddEntityFramework(Configuration)
 				.AddSqlServer()
-				.AddDbContext<MoviesAppContext>(options =>
-				{
-					options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MoviesDatabase;Trusted_Connection=True");
-				});
+				.AddDbContext<MoviesAppContext>();
 		}
 
 		public void Configure(IApplicationBuilder app)
